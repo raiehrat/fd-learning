ad556fffb987b215d6a08560110d6e9c0e626323
Rai Ehrat <rai_ehrat@hotmail.com>
2020-11-30 19:53:41 -0300

    Tempo 3

--- pages/tempo.js
+++ pages/tempo.js
@@ -13,19 +13,17 @@ function Tempo(props) {
 
 export async function getStaticProps(){
     console.log('> Passando pelo GetStaticProps();');
-    console.log('> Adicionando delay de 5 segundos');
 
-    await delay(5000);
     const staticDate = new Date();
     const staticDateString = staticDate.toGMTString();
 
     return {
         props: {
             staticDateString
-        }
+        },
+        revalidate: 1
     }
 }
 
-const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default Tempo;
