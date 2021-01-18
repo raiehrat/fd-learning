2ed7fb4914d2754fe863d6d220b468cd4b54f26d
Rai Ehrat <rai_ehrat@hotmail.com>
2021-01-18 15:10:13 -0300

    home-alteracao

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 function Home(){
     return (<div>
-        <h1>Home para o RODA</h1>
+        <h1>Home</h1>
         <Link href='/sobre'>
             <a>Acessar página Sobre</a>
         </Link>        
